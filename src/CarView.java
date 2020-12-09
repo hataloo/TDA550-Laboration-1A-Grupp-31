6a1bad58dd2b70ed1cd27995bf65ee7cfb6a34fd
hataloo <jacksandberg998@gmail.com>
2020-12-09 10:41:02 +0100

    The storm, part 2. Implemented CarView.

--- src/CarView.java
+++ src/CarView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class CarView extends JPanel implements CarObserver{
@@ -7,10 +8,39 @@ public class CarView extends JPanel implements CarObserver{
 
     public CarView(JFrame frame) {
         this.frame = frame;
+        this.setDoubleBuffered(true);
+        this.setPreferredSize(new Dimension(frame.getX(), frame.getY()-240));
+        this.setBackground(Color.green);
+        this.frame.add(this);
+    }
+
+    public CarView(JFrame frame, int x, int y) {
+        this.frame = frame;
+        this.setDoubleBuffered(true);
+        this.setPreferredSize(new Dimension(x,y));
+        this.setBackground(Color.green);
+        this.frame.add(this);
     }
 
     @Override
     public void actOnVehicleMovement(List<VehicleImage> vehicles) {
-
+        this.vehicles = vehicles;
+        this.frame.repaint();
     }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for(VehicleImage vehicle : vehicles){
+            Point point = convertCoordinatesToPoint(vehicle.getVehicle());
+            g.drawImage(vehicle.getImage(), (int) point.getX(), (int) point.getY(), null);
+        }
+    }
+
+    private Point convertCoordinatesToPoint(Vehicle vehicle){
+        int x = (int) Math.round(vehicle.getXPosition());
+        int y = (int) Math.round(vehicle.getYPosition());
+        return new Point(x,y);
+    }
+
 }
